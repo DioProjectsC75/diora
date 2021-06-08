f798f5a370ce238a1165d3b516b16336f59c7ff5
Carlos Millett <carlos4735@gmail.com>
2021-06-08 00:25:41 -0300

    add mapping to convert data between DTO and entity format

---
+++ src/main/java/o/di/diora/mapper/PersonMapper.java
@@ -0,0 +1,17 @@
+package o.di.diora.mapper;
+
+import o.di.diora.dto.request.PersonDTO;
+import o.di.diora.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
--- src/main/java/o/di/diora/service/PersonService.java
+++ src/main/java/o/di/diora/service/PersonService.java
@@ -3,6 +3,7 @@ package o.di.diora.service;
 import o.di.diora.dto.request.PersonDTO;
 import o.di.diora.dto.response.MessageResponseDTO;
 import o.di.diora.entity.Person;
+import o.di.diora.mapper.PersonMapper;
 import o.di.diora.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,14 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
-        Person savedPerson = personRepository.save(personDTO);
+        Person savedPerson = personMapper.toModel(personDTO);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID:" + savedPerson.getId())
