01f43831e292829af00305ce526b49b0dac39e20
Carlos Millett <carlos4735@gmail.com>
2021-06-08 16:57:37 -0300

    implement updateById() method
    enable update of database entries using the API

--- src/main/java/o/di/diora/service/PersonService.java
+++ src/main/java/o/di/diora/service/PersonService.java
@@ -25,10 +25,7 @@ public class PersonService {
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person savedPerson = personMapper.toModel(personDTO);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID:" + savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID: ");
     }
 
     public List<PersonDTO> listAll() {
@@ -49,7 +46,20 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+        Person updatedPerson = personMapper.toModel(personDTO);
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID: ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 }
