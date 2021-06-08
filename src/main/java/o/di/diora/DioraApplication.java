1eb31aab4161d58020a31b51cc2125bab251db99
Carlos Millett <carlos4735@gmail.com>
2021-06-07 23:39:50 -0300

    add spring boot project files
    Project created using https://start.spring.io
    
    selected dependencies:
     - Spring Web
     - Spring Data JPA
     - H2 Database
     - Validation
     - Lombok
    
    'Validation' needs to be explicitly added since Spring Boot version 2.3.1

---
+++ src/main/java/o/di/diora/DioraApplication.java
@@ -0,0 +1,13 @@
+package o.di.diora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/o/di/diora/DioraApplicationTests.java
@@ -0,0 +1,13 @@
+package o.di.diora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
