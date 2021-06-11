ac6fa46a4d41648f94997ffed978ff1d39bac6b5
Carlos Millett <carlos4735@gmail.com>
2021-06-11 01:29:21 -0300

    add tests to mapper

---
+++ src/test/java/o/di/diora/mapper/PersonMapperTest.java
@@ -0,0 +1,50 @@
+package o.di.diora.mapper;
+
+import o.di.diora.dto.request.PersonDTO;
+import o.di.diora.dto.request.PhoneDTO;
+import o.di.diora.entity.Person;
+import o.di.diora.entity.Phone;
+import o.di.diora.utils.PersonUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonMapperTest {
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Test
+    void testGivenPersonDTOThenReturnPersonEntity() {
+        PersonDTO personDTO = PersonUtils.createFakeDTO();
+        Person person = personMapper.toModel(personDTO);
+
+        assertEquals(personDTO.getFirstName(), person.getFirstName());
+        assertEquals(personDTO.getLastName(), person.getLastName());
+        assertEquals(personDTO.getCpf(), person.getCpf());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phoneDTO.getType(), phone.getType());
+        assertEquals(phoneDTO.getNumber(), phone.getNumber());
+    }
+
+    @Test
+    void testGivenPersonEntityThenReturnPersonDTO() {
+        Person person = PersonUtils.createFakeEntity();
+        PersonDTO personDTO = personMapper.toDTO(person);
+
+        assertEquals(person.getFirstName(), personDTO.getFirstName());
+        assertEquals(person.getLastName(), personDTO.getLastName());
+        assertEquals(person.getCpf(), personDTO.getCpf());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phone.getType(), phoneDTO.getType());
+        assertEquals(phone.getNumber(), phoneDTO.getNumber());
+    }
+}
