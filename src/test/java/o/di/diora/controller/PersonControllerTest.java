3b124727e0397046679a047208bc04a76ad1a5fe
Carlos Millett <carlos4735@gmail.com>
2021-06-11 01:27:27 -0300

    add tests to controller
    includes new dependency to build.gradle

---
+++ src/test/java/o/di/diora/controller/PersonControllerTest.java
@@ -0,0 +1,136 @@
+package o.di.diora.controller;
+
+import o.di.diora.dto.request.PersonDTO;
+import o.di.diora.dto.response.MessageResponseDTO;
+import o.di.diora.exception.PersonNotFoundException;
+import o.di.diora.service.PersonService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.Collections;
+import java.util.List;
+
+import static o.di.diora.utils.PersonUtils.asJsonString;
+import static o.di.diora.utils.PersonUtils.createFakeDTO;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonControllerTest {
+    private static final String URL_PATH = "/api/v1/diora";
+    private MockMvc mockMvc;
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
+    @BeforeEach
+    void setUp() {
+        personController = new PersonController(personService);
+        mockMvc = MockMvcBuilders.standaloneSetup(personController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void testWhenPOSTIsCalledThenAPersonShouldBeCreated() throws Exception {
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        MessageResponseDTO expectedResponseMessage = createMessageResponse("Created person with ID: ", 1L);
+
+        when(personService.createPerson(expectedPersonDTO)).thenReturn(expectedResponseMessage);
+
+        mockMvc.perform(post(URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedPersonDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message", is(expectedResponseMessage.getMessage())));
+    }
+
+    @Test
+    void testWhenGETWithValidIsCalledThenAPersonShouldBeReturned() throws Exception {
+        Long expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+
+        when(personService.findById(expectedValidId)).thenReturn(expectedPersonDTO);
+
+        mockMvc.perform(get(URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstName", is("Joao")))
+                .andExpect(jsonPath("$.lastName", is("Ninguem")));
+    }
+
+    @Test
+    void testWhenGETWithInvalidIsCalledThenAnErrorMessageShouldBeReturned() throws Exception {
+        Long expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+
+        when(personService.findById(expectedValidId)).thenThrow(PersonNotFoundException.class);
+
+        mockMvc.perform(get(URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testWhenGETIsCalledThenPeopleListShouldBeReturned() throws Exception {
+        Long expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        expectedPersonDTO.setId(expectedValidId);
+        List<PersonDTO> expectedPeopleDTOList = Collections.singletonList(expectedPersonDTO);
+
+        when(personService.listAll()).thenReturn(expectedPeopleDTOList);
+
+        mockMvc.perform(get(URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].firstName", is("Joao")))
+                .andExpect(jsonPath("$[0].lastName", is("Ninguem")));
+    }
+
+    @Test
+    void testWhenPUTIsCalledThenAPersonShouldBeUpdated() throws Exception {
+        long expectedValidId = 1L;
+        PersonDTO expectedPersonDTO = createFakeDTO();
+        MessageResponseDTO expectedResponseMessage = createMessageResponse("Updated person with ID: ", 1L);
+
+        when(personService.updateById(expectedValidId, expectedPersonDTO)).thenReturn(expectedResponseMessage);
+
+        mockMvc.perform(put(URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedPersonDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", is(expectedResponseMessage.getMessage())));
+    }
+
+    @Test
+    void testWhenDELETEIsCalledThenAPersonShouldBeDeleted() throws Exception {
+        Long expectedValidId = 1L;
+
+        mockMvc.perform(delete(URL_PATH + "/" + expectedValidId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    private MessageResponseDTO createMessageResponse(String message, Long id) {
+        return MessageResponseDTO.builder()
+                .message(message + id)
+                .build();
+    }
+}
--- src/test/java/o/di/diora/utils/PhoneUtils.java
+++ src/test/java/o/di/diora/utils/PhoneUtils.java
@@ -5,7 +5,7 @@ import o.di.diora.entity.Phone;
 import o.di.diora.enums.PhoneType;
 
 public class PhoneUtils {
-    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final String PHONE_NUMBER = "(11)99999-9999";
     private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
     private static final long PHONE_ID = 1L;
 
